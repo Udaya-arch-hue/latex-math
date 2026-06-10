ec748ae17cb3bb811c460f6af5622ff582504ba0
Udaya Sree <pudayasree2005@gmail.com>
2026-06-10 12:20:11 +0530

    Updated Gate7474 logic

--- gate/Gate7474/src/Gate7474.cpp
+++ gate/Gate7474/src/Gate7474.cpp
@@ -1,26 +1,37 @@
 #include <Arduino.h>
-void disp_7474(int D,int C,int B,int A){
-digitalWrite(2,A);
-digitalWrite(3,B);
-digitalWrite(4,C);
-digitalWrite(5,D);
+
+void disp_7474(int D,int C,int B,int A)
+{
+  digitalWrite(2,A);
+  digitalWrite(3,B);
+  digitalWrite(4,C);
+  digitalWrite(5,D);
 }
-void setup(){
-pinMode(2,OUTPUT);
-pinMode(3,OUTPUT);
-pinMode(4,OUTPUT);
-pinMode(5,OUTPUT);
+
+void setup()
+{
+  pinMode(2,OUTPUT);
+  pinMode(3,OUTPUT);
+  pinMode(4,OUTPUT);
+  pinMode(5,OUTPUT);
 }
-void loop(){
-int P=0;
-int Q=0;
-int R=0;
-int F=(!Q)&&((!P)||(!R));
-if(F==1){
-disp_7474(0,0,0,1);
-}
-else{
-disp_7474(0,0,0,0);
-}
-delay(1000);
+
+void loop()
+{
+  int P=0;
+  int Q=0;
+  int R=0;
+
+  int F=(!Q)&&(!R);
+
+  if(F==1)
+  {
+    disp_7474(0,0,0,1);
+  }
+  else
+  {
+    disp_7474(0,0,0,0);
+  }
+
+  delay(1000);
 }
