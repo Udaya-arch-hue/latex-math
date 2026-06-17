8577aa749b95a431397dd21d98f494d120220378
Udaya Sree <pudayasree2005@gmail.com>
2026-06-17 14:02:20 +0530

    Updated q9_xor code

--- Pico2W/Seven_Segment/q9_xor.ino
+++ Pico2W/Seven_Segment/q9_xor.ino
@@ -1,4 +1,5 @@
 #include <Arduino.h>
+
 void sevenseg(int a,int b,int c,int d,int e,int f,int g)
 {
 digitalWrite(2,a);
@@ -9,24 +10,29 @@ digitalWrite(6,e);
 digitalWrite(7,f);
 digitalWrite(8,g);
 }
+
 void setup()
 {
 for(int i=2;i<=8;i++)
 pinMode(i,OUTPUT);
+
 pinMode(10,INPUT_PULLDOWN);
 pinMode(11,INPUT_PULLDOWN);
 pinMode(12,INPUT_PULLDOWN);
 }
+
 void loop()
 {
-int p=digitalRead(10);
-int q=digitalRead(11);
-int r=digitalRead(12);
-int f=p^q^r;
-if(f)
+int P=digitalRead(10);
+int Q=digitalRead(11);
+int R=digitalRead(12);
+
+int F=P^Q^R;
+
+if(F==1)
 sevenseg(1,0,0,1,1,1,1);
 else
 sevenseg(0,0,0,0,0,0,1);
+
 delay(100);
 }
-
