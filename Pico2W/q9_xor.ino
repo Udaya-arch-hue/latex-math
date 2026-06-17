379e30707d7ea7ca22fd7b88e9ae6d5d0176ffa5
Udaya Sree <pudayasree2005@gmail.com>
2026-06-17 13:40:31 +0530

    Added Pico2W files

---
+++ Pico2W/q9_xor.ino
@@ -0,0 +1,32 @@
+#include <Arduino.h>
+void sevenseg(int a,int b,int c,int d,int e,int f,int g)
+{
+digitalWrite(2,a);
+digitalWrite(3,b);
+digitalWrite(4,c);
+digitalWrite(5,d);
+digitalWrite(6,e);
+digitalWrite(7,f);
+digitalWrite(8,g);
+}
+void setup()
+{
+for(int i=2;i<=8;i++)
+pinMode(i,OUTPUT);
+pinMode(10,INPUT_PULLDOWN);
+pinMode(11,INPUT_PULLDOWN);
+pinMode(12,INPUT_PULLDOWN);
+}
+void loop()
+{
+int p=digitalRead(10);
+int q=digitalRead(11);
+int r=digitalRead(12);
+int f=p^q^r;
+if(f)
+sevenseg(1,0,0,1,1,1,1);
+else
+sevenseg(0,0,0,0,0,0,1);
+delay(100);
+}
+
